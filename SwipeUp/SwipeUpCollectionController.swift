f4af87ef424263e9b49ad16b121c86b8d4aa8055
Robert Otani <robert@otanistudio.com>
2015-06-06 18:17:48 -0700

    reset data source when all items are removed

--- SwipeUp/SwipeUpCollectionController.swift
+++ SwipeUp/SwipeUpCollectionController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class SwipeUpCollectionController: UICollectionViewController, SwipeUpCellDelegate {
 
-    var items: NSMutableArray =  NSMutableArray(array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    var items: NSMutableArray?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        reset()
     }
 
     // MARK: UICollectionViewDataSource
@@ -23,16 +24,20 @@ class SwipeUpCollectionController: UICollectionViewController, SwipeUpCellDelega
     }
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
+        return items!.count
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(SwipeUpCell.cellID(), forIndexPath: indexPath) as! SwipeUpCell
-        let tagNumber = items[indexPath.item] as! NSNumber
+        let tagNumber = items![indexPath.item] as! NSNumber
         cell.configure(self, tag: tagNumber)
         // Configure the cell
     
         return cell
+    }
+    
+    private func reset() {
+        items = NSMutableArray(array: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     }
     
     // MARK: SwipeUpCellDelegate
@@ -40,10 +45,15 @@ class SwipeUpCollectionController: UICollectionViewController, SwipeUpCellDelega
     func swipeUpDidFinish(tag: NSNumber) {
         let lock = dispatch_queue_create("com.otanistudio.SwipeUp.lockQueue", nil)
         dispatch_sync(lock, { () -> Void in
-            let indexToRemove = self.items.indexOfObject(tag)
+            let indexToRemove = self.items!.indexOfObject(tag)
             let indexPathToRemove = NSIndexPath(forItem: indexToRemove, inSection: 0)
-            self.items.removeObjectAtIndex(indexToRemove)
-            self.collectionView?.deleteItemsAtIndexPaths([indexPathToRemove])
+            self.items!.removeObjectAtIndex(indexToRemove)
+            self.collectionView!.deleteItemsAtIndexPaths([indexPathToRemove])
+            
+            if (self.items!.count == 0) {
+                self.reset()
+                self.collectionView!.reloadData()
+            }
         })
     }
 
